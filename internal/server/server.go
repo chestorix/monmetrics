224ed5af6d2ba317336d34987908f39d395d9796
vav1 <vav1@vgserv.ru>
2025-04-20 13:30:45 +0500

    add chi router

--- internal/server/server.go
+++ internal/server/server.go
@@ -5,6 +5,7 @@ import (
 	"github.com/chestorix/monmetrics/internal/config"
 	"github.com/chestorix/monmetrics/internal/handlers"
 	"github.com/chestorix/monmetrics/internal/storage/interfaces"
+	"github.com/go-chi/chi/v5"
 	"net/http"
 )
 
@@ -12,26 +13,40 @@ type Server struct {
 	cfg    *config.ServerConfig
 	repo   interfaces.MetricsRepository
 	server *http.Server
+	router *chi.Mux
 }
 
 func New(cfg *config.ServerConfig, repo interfaces.MetricsRepository) *Server {
 	return &Server{
-		cfg:  cfg,
-		repo: repo,
+		cfg:    cfg,
+		repo:   repo,
+		router: chi.NewRouter(),
 	}
+}
+
+func (s *Server) setupRoutes(handler *handlers.MetricsHandler) {
+	s.router.Route("/", func(r chi.Router) {
+		r.Get("/", handler.GetAllMetricsHandler)
+
+		r.Route("/update", func(r chi.Router) {
+			r.Post("/{metricType}/{metricName}/{metricValue}", handler.UpdateHandler)
+		})
+
+		r.Route("/value", func(r chi.Router) {
+			r.Get("/{metricType}/{metricName}", handler.GetValuesHandler)
+		})
+	})
 }
 
 func (s *Server) Start() error {
 	handler := handlers.NewMetricsHandler(s.repo)
+	s.setupRoutes(handler)
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/update/", handler.UpdateHandler)
-	mux.HandleFunc("/value/", handler.GetValuesHandler)
-	mux.HandleFunc("/", handler.GetAllMetricsHandler)
 	s.server = &http.Server{
 		Addr:    s.cfg.Address,
-		Handler: mux,
+		Handler: s.router,
 	}
-	fmt.Println("Server is listening on http://localhost:8080")
+
+	fmt.Printf("Server is listening on %s\n", s.cfg.Address)
 	return s.server.ListenAndServe()
 }
