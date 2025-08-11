24a92709d741d62a7ff14a3575a29b565ed6641d
unknown <chestorix@gmail.com>
2025-08-11 13:46:15 +0500

    fix

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,18 +1,21 @@
+// Package config содержит конфигурационные структуры для сервера и агента.
 package config
 
 import "time"
 
 // ServerConfig содержит конфигурационные параметры сервера.
+
 type ServerConfig struct {
-	Address         string        // Адрес сервера (поумоланию  ":8080")
-	StoreInterval   time.Duration // Интервал сохранения данных на диск (0  синхронно )
-	FileStoragePath string        // Путь до файла сохранения метрик
-	Restore         bool          // Восстановление данных при запуске из хранилища
-	DatabaseDSN     string        // Строка подклбчения к Базе данных
-	Key             string        // Ключ для генерации ХЕШ
+	Address         string        // адрес и порт сервера (например: ":8080")
+	StoreInterval   time.Duration // интервал сохранения метрик на диск (0 - синхронная запись)
+	FileStoragePath string        // путь к файлу для хранения метрик
+	Restore         bool          // восстанавливать метрики из файла при старте
+	DatabaseDSN     string        // строка подключения к БД (если используется)
+	Key             string        // секретный ключ для проверки хешей
 }
 
 // AgentConfig содержит конфигурационные параметры агента.
+
 type AgentConfig struct {
 	Address        string        // Адрес сервера для подключения
 	PollInterval   time.Duration // Интервал опроса метрик
