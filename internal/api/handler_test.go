ce6a27a19888ae02a653dad3d4acb7f9f68157a0
vav1 <vav1@vgserv.ru>
2025-09-04 10:57:02 +0500

    fix

--- internal/api/handler_test.go
+++ internal/api/handler_test.go
@@ -246,7 +246,7 @@ func TestMetricsHandler_UpdateHandler(t *testing.T) {
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			mockService := NewMockMetricsService()
-			handler := NewMetricsHandler(mockService, "", "")
+			handler := NewMetricsHandler(mockService, "", "", "")
 
 			request := httptest.NewRequest(http.MethodPost, test.url, nil)
 			w := httptest.NewRecorder()
@@ -332,7 +332,7 @@ func TestMetricsHandler_GetValuesHandler(t *testing.T) {
 			if test.prepare != nil {
 				test.prepare(mockService)
 			}
-			handler := NewMetricsHandler(mockService, "", "")
+			handler := NewMetricsHandler(mockService, "", "", "")
 
 			request := httptest.NewRequest(http.MethodGet, test.url, nil)
 			w := httptest.NewRecorder()
@@ -405,7 +405,7 @@ func TestMetricsHandler_GetAllMetricsHandler(t *testing.T) {
 			if test.prepare != nil {
 				test.prepare(mockService)
 			}
-			handler := NewMetricsHandler(mockService, "", "")
+			handler := NewMetricsHandler(mockService, "", "", "")
 
 			request := httptest.NewRequest(http.MethodGet, "/", nil)
 			w := httptest.NewRecorder()
@@ -468,7 +468,7 @@ func TestMetricsHandler_UpdateJSONHandler(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			mockService := NewMockMetricsService()
-			handler := NewMetricsHandler(mockService, "", "")
+			handler := NewMetricsHandler(mockService, "", "", "")
 
 			req := httptest.NewRequest(http.MethodPost, "/update/", strings.NewReader(tt.payload))
 			req.Header.Set("Content-Type", "application/json")
@@ -531,7 +531,7 @@ func TestMetricsHandler_ValueJSONHandler(t *testing.T) {
 			if tt.prepare != nil {
 				tt.prepare(mockService)
 			}
-			handler := NewMetricsHandler(mockService, "", "")
+			handler := NewMetricsHandler(mockService, "", "", "")
 
 			req := httptest.NewRequest(http.MethodPost, "/value/", strings.NewReader(tt.payload))
 			req.Header.Set("Content-Type", "application/json")
@@ -582,7 +582,7 @@ func TestMetricsHandler_PingHandler(t *testing.T) {
 			mockService := NewMockMetricsService()
 			mockService.checkDBError = tt.mockError
 
-			handler := NewMetricsHandler(mockService, tt.dbDNS, "")
+			handler := NewMetricsHandler(mockService, tt.dbDNS, "", "")
 
 			req := httptest.NewRequest(http.MethodGet, "/ping", nil)
 			w := httptest.NewRecorder()
